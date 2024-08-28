9b1eaad821ea30750164d258b0dc05cba9cb00fc
GideonAmhaG <gideonamha@gmail.com>
2024-08-28 13:48:11 +0300

    completed create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,7 +19,7 @@ import {
   signoutUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
-import { set } from "mongoose";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -193,6 +193,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Save"}
         </button>
+        <Link
+          to={"/create-listing"}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
